b734016639a354fb4ec514f19a72b7603f5b8000
agus-darmawan <wayanagus.dr@gmail.com>
2025-06-12 21:01:54 +0700

    feat: fix build directory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,18 @@
-export default {
+// @ts-eslint-disable
+module.exports = {
  apps: [
   {
-   name: "patrol-management-system",
+   name: "patrol-be",
    script: "./dist/bin/www.js",
    instances: "max",
    exec_mode: "cluster",
    env: {
     NODE_ENV: "production",
-    PORT: 3000,
+    PORT: 3001,
    },
    env_production: {
     NODE_ENV: "production",
-    PORT: 3000,
+    PORT: 3001,
    },
    error_file: "./logs/err.log",
    out_file: "./logs/out.log",
